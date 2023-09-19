82f64d31a442a87d84ec3a8c438a7b8267099c24
Brian Kibiku <kibikukimani2@gmail.com>
2023-09-19 12:54:28 +0300

    feat: add app user profile page

--- lib/features/profile/presentation/pages/profile.dart
+++ lib/features/profile/presentation/pages/profile.dart
@@ -9,7 +9,41 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppBar(context, 'Profile'),
-      body: const Text('Profile'),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage(
+                  'assets/images/woman.png'), // Replace with your image path
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Jane Doe',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Email: janedoe@moniepoint.com',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Role: Logistics Manager',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
